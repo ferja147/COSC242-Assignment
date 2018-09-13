08fb96877685da3b02bd419a51abc15ec9ee3d6d
OxyOCE <harry.smith.0119@gmail.com>
2018-09-13 16:41:05 +1200

    Comments checked

--- tree.c
+++ tree.c
@@ -8,6 +8,8 @@
 
 static tree_t tree_type;
 
+
+/* Generate tree struct. */
 struct tree_node {
     char *key;
     tree_colour colour;
@@ -16,26 +18,10 @@ struct tree_node {
     int frequency;
 };
 
-tree right_rotate(tree t)
-{
- * @param t the tree to output the DOT description of.
- * @param out the stream to write the DOT description to.
- */
-void tree_output_dot(tree t, FILE *out) {
-    fprintf(out, "digraph tree {\nnode [shape = Mrecord, penwidth = 2];\n");
-    tree_output_dot_aux(t, out);
-    fprintf(out, "}\n");
-}
-    tree tmp = t;
-    t = t->left;
-    tmp->left = t->right;
-    t->right = tmp;
-    return t;
-}
 /**
- * Used by red black trees to rotate the tree to the left.
- * @param t tree to rotate.
- * @return rotated tree.
+ * Rotate the nodes of an RBT tree to the left.
+ * @param t a given tree to rotate
+ * @return the updated tree
  */
 tree left_rotate(tree t)
 {
@@ -45,221 +31,222 @@ tree left_rotate(tree t)
     t->left = tmp;
     return t;
 }
-
-/*static tree flipColour(tree b) {
-    b->colour = RED;
-    b->left->colour = BLACK;
-    b->right->colour = BLACK;
-    return b;
-    }*/
-
-
-tree tree_new(tree_t type) {#include "tree.h"
-#include <string.h>
-#include <stdlib.h>
-#include "mylib.h"
-
-#define IS_BLACK(x) ((NULL == (x)) || (BLACK == (x)->colour))
-#define IS_RED(x) ((NULL != (x)) && (RED == (x)->colour))
-
-static tree_t tree_type;
-
-struct tree_node {
-    char *key;
-    tree_colour colour;
-    tree left;
-    tree right;
-    int frequency;
-};
-
+/**
+ * Rotate the nodes of an RBT tree to the right.
+ * @param t a given tree to rotate
+ * @return the updated tree
+ */
 tree right_rotate(tree t)
 {
- * @param t the tree to output the DOT description of.
- * @param out the stream to write the DOT description to.
- */
-void tree_output_dot(tree t, FILE *out) {
-    fprintf(out, "digraph tree {\nnode [shape = Mrecord, penwidth = 2];\n");
-    tree_output_dot_aux(t, out);
-    fprintf(out, "}\n");
-}
     tree tmp = t;
     t = t->left;
     tmp->left = t->right;
     t->right = tmp;
     return t;
 }
+
+
 /**
- * Used by red black trees to rotate the tree to the left.
- * @param t tree to rotate.
- * @return rotated tree.
+ * Create a new tree.
+ * @param type used to define what tree the program creates
+ * @return the newly created tree with a user-defined type
  */
-tree left_rotate(tree t)
+tree tree_new(tree_t type)
 {
-    tree tmp = t;
-    t = t->right;
-    tmp->right = t->left;
-    t->left = tmp;
-    return t;
-}
-
-/*static tree flipColour(tree b) {
-    b->colour = RED;
-    b->left->colour = BLACK;
-    b->right->colour = BLACK;
-    return b;
-    }*/
-
-
-tree tree_new(tree_t type) {
-    tree b = emalloc(sizeof(struct tree_node));
-    b->key = NULL;
-    b->left = NULL;
-    b->right = NULL;
+    
     tree_type = type;
-
-    b->colour = RED;
-    b->frequency = 0;
-    
-    return b;
+    return NULL;    
 }
 
-tree tree_fix(tree b){
-    if(IS_RED(b->left) && IS_RED(b->left->left)){
-        if (IS_RED(b->right)){
-            /*colour root as red and children a and b black*/
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }else if(IS_BLACK(b->right)){
-            /*right rotate root , colour new root (a) black,
-             * and new child(old root) red*/
-            b = right_rotate(b);
-            b->colour = BLACK;
-            b->right->colour = RED;/* Old root*/
-        }
-    }
-
-    else if(IS_RED(b->left) && IS_RED(b->left->right)){
-        if (IS_RED(b->right)){
-            /*colours root red and children a,b as black*/
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }
-        else if(IS_BLACK(b->right)){
-            b->left = left_rotate(b->left);
-            b = right_rotate(b);
-            b->colour = BLACK;
-            b->right->colour = RED;
-        }
-    }
-
-    else if(IS_RED(b->right) && IS_RED(b->right->left)){
-        if (IS_RED(b->left)){
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }else if(IS_BLACK(b->left)){
-            b->right = right_rotate(b->right);
-            b = left_rotate(b);
-            b->colour = BLACK;
-            b->left->colour= RED;
-        }
-    }
-
-    else if (IS_RED(b->right) && IS_RED(b->right->right)){
-        if (IS_RED(b->left)){
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }else{
-            b = left_rotate(b);
-            b->colour = BLACK;
-            b->left->colour = RED;
-        }
-    }
-
-    return b;
-}
-
-tree tree_insert(tree b, char *str) {
-    int cmp;
-    if (b == NULL) {
-        b = tree_new(tree_type);
-    }
-    
-    if (b->key == NULL) {
-        b->key = emalloc(sizeof(char) * (strlen(str) + 1));
-        strcpy(b->key, str);
-        b->frequency = 1;
-        return b;
-    }
-    
-    cmp = strcmp(str, b->key);
-    if (cmp < 0) {
-        b->left = tree_insert(b->left, str);
-    } else if (cmp > 0) {
-        b->right = tree_insert(b->right, str);
-    } else if (cmp == 0) {
-        b->frequency += 1;
-    }
-    
-    if (tree_type == RBT) {
-        b = tree_fix(b);
-    }
-    
-    return b;
-}
-
-/*
- * returns 0 if not found
- * returns 1 if found
+/**
+ * Flip the colours of the tree so that children are
+ * BLACK with RED roots.
+ * @param t a given tree to flip colours
+ * @return return a post-flip tree
  */
-int tree_search(tree b, char *str) {
-    if (b == NULL) {
+static tree flipColour(tree t){
+    t->colour = RED;
+    t->left->colour = BLACK;
+    t->right->colour = BLACK;
+    return t;
+}
+
+/**
+ * Fix trees when their structure is altered.
+ * @param t a given tree requiring restructuring
+ * @return the post-fix tree
+ */
+tree tree_fix(tree b){
+
+    if (IS_RED(b->left) && IS_RED(b->left->left) && IS_RED(b->right)){
+        b = flipColour(b);
+    }
+    if(IS_RED(b->left) && IS_RED(b->left->left) && IS_BLACK(b->right)){
+        b = right_rotate(b);
+        b->colour = BLACK;
+        b->left->colour = RED;
+        b->right->colour = RED;
+    }
+    if(IS_RED(b->left) && IS_RED(b->left->right) && IS_RED(b->right)) {
+        b = flipColour(b);
+    }
+    if(IS_RED(b->left) && IS_RED(b->left->right) && IS_BLACK(b->right)) {
+        b->left = left_rotate(b->left);
+        b = right_rotate(b);
+        b->colour = BLACK;
+        b->left->colour = RED;
+        b->right->colour = RED;
+    }
+    if(IS_RED(b->right) && IS_RED(b->right->left) && IS_RED(b->left)) {
+        b = flipColour(b);
+    }
+    if(IS_RED(b->right) && IS_RED(b->right->left) && IS_BLACK(b->left)) {
+        b->right = right_rotate(b->right);
+        b = left_rotate(b);
+        b->colour = BLACK;
+        b->left->colour = RED;
+        b->right->colour = RED;
+    }
+    if(IS_RED(b->right) && IS_RED(b->right->right) && IS_RED(b->left)) {
+        b = flipColour(b);
+    }
+    if(IS_RED(b->right) && IS_RED(b->right->right) && IS_BLACK(b->left)) {
+        ;
+        b = left_rotate(b);
+        b->colour = BLACK;
+        b->left->colour = RED;
+        b->right->colour = RED;
+    }
+    return b;
+    
+}
+/**
+ * Used to set root to black.
+ * @param t a given tree to set root black.
+ */
+tree setColourBlack (tree t) {
+    t->colour = BLACK;
+    return t;
+}
+
+/**
+ * Insert a value into a given tree.
+ * @param t a given tree to insert a value into
+ * @param str the value to be inserted into the tree
+ */
+static tree tree_insert_wrapper(tree t, char *str) {
+    if (t == NULL){
+        
+        tree t = emalloc(sizeof *t);
+        t->key = emalloc((strlen(str) +1) * sizeof(char));
+        
+        strcpy(t->key, str);
+        
+        t->frequency++;
+        t->left = NULL;
+        t->right = NULL;
+        
+        if (tree_type == RBT) {
+            t = tree_fix(t);
+        }
+        
+        return t;
+        
+    }
+    
+    else if (strcmp(t->key, str) == 0){
+        t->frequency++;
+        return t;
+    }
+    else if (strcmp(str, t->key) > 0){
+        t->right = tree_insert_wrapper(t->right, str);
+    }
+    else{
+        t->left = tree_insert_wrapper(t->left, str);
+    }
+    
+    if(tree_type == RBT)
+        {
+            t = tree_fix(t);
+        }
+    return t;
+}
+
+/**
+ * Wrapper function for recursive call of tree_insert_wrapper().
+ * @param t a given tree to insert a value into
+ * @param str the value to be inserted into the tree
+ */
+tree tree_insert (tree t, char *str) {
+    t = tree_insert_wrapper(t, str);
+    setColourBlack(t);
+    return t;
+}
+
+/**
+ * Search a given tree for a value given by the user. Return 1 if the value
+ * is found, 0 otherwise.
+ * @param b a given tree to search for values
+ * @param str the value to search the tree for
+ * @return an integer to indicate search results. 1 for success, 0 for failure.
+ */
+int tree_search(tree t, char *str) {
+    if (t == NULL) {
         return 0;
     }
-    if (strcmp(str, b->key) == 0) {
+    if (strcmp(str, t->key) == 0) {
         return 1;
-    } else if (strcmp(str, b->key) < 0) {
-        return tree_search(b->left, str);
+    } else if (strcmp(str, t->key) < 0) {
+        return tree_search(t->left, str);
     } else {
-        return tree_search(b->right, str);
+        return tree_search(t->right, str);
     }
 }
 
-void tree_inorder(tree b, void f(char *str)) {
-    if (b == NULL) {
+/**
+ * Traverses a tree in in-order fashion.
+ * @param b a given tree to traverse
+ * @param f a function given a value to traverse
+ */
+void tree_inorder(tree t, void f(char *str)) {
+    if (t == NULL) {
         return;
     }
-    tree_inorder(b->left, f);
-    f(b->key);
-    tree_inorder(b->right, f);
+    tree_inorder(t->left, f);
+    f(t->key);
+    tree_inorder(t->right, f);
 }
 
-void tree_preorder(tree b, void f(int frequency, char *str)) {
-    if (b == NULL) {
+/**
+ * Traverse a tree in pre-order fashion.
+ * @param b a given tree to traverse
+ * @param f a function given a word frequency
+ * value as well as a key value to traverse
+ */
+void tree_preorder(tree t, void f(int frequency, char *str)) {
+    if (t == NULL) {
         return;
     }
-    f(b->frequency, b->key);
-    tree_preorder(b->left, f);
-    tree_preorder(b->right, f);
+    f(t->frequency, t->key);
+    tree_preorder(t->left, f);
+    tree_preorder(t->right, f);
 }
 
-tree setColourBlack(tree b) {
-    b->colour = BLACK;
-    return b;
-}
 
-tree tree_free(tree b) {
-    if (b == NULL){
-        return b;
+/**
+ * Free the memory allocated to a given tree.
+ * @param b a given tree to free memory from
+ * @return a tree freed from memory
+ */
+tree tree_free(tree t) {
+    if (t == NULL){
+        return t;
     }else{
-        tree_free(b->left);
-        tree_free(b->right);
-        free(b->key);
-        free(b);
-        return b;
+        tree_free(t->left);
+        tree_free(t->right);
+        free(t->key);
+        free(t);
+        return t;
     }
 }
 
@@ -270,7 +257,7 @@ tree tree_free(tree b) {
  * @param t the tree to output a DOT description of.
  * @param out the stream to write the DOT output to.
  */
-void tree_output_dot_aux(tree t, FILE *out) {
+static void tree_output_dot_aux(tree t, FILE *out) {
     if(t->key != NULL) {
         fprintf(out, "\"%s\"[label=\"{<f0>%s:%d|{<f1>|<f2>}}\"color=%s];\n",
                 t->key, t->key, t->frequency,
@@ -295,187 +282,11 @@ void tree_output_dot_aux(tree t, FILE *out) {
  *
  * You can also use png, ps, jpg, svg... instead of pdf
  *
-    tree b = emalloc(sizeof(struct tree_node));
-    b->key = NULL;
-    b->left = NULL;
-    b->right = NULL;
-    tree_type = type;
-
-    b->colour = RED;
-    b->frequency = 0;
-    
-    return b;
-}
-
-tree tree_fix(tree b){
-    if(IS_RED(b->left) && IS_RED(b->left->left)){
-        if (IS_RED(b->right)){
-            /*colour root as red and children a and b black*/
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }else if(IS_BLACK(b->right)){
-            /*right rotate root , colour new root (a) black,
-             * and new child(old root) red*/
-            b = right_rotate(b);
-            b->colour = BLACK;
-            b->right->colour = RED;/* Old root*/
-        }
-    }
-
-    else if(IS_RED(b->left) && IS_RED(b->left->right)){
-        if (IS_RED(b->right)){
-            /*colours root red and children a,b as black*/
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }
-        else if(IS_BLACK(b->right)){
-            b->left = left_rotate(b->left);
-            b = right_rotate(b);
-            b->colour = BLACK;
-            b->right->colour = RED;
-        }
-    }
-
-    else if(IS_RED(b->right) && IS_RED(b->right->left)){
-        if (IS_RED(b->left)){
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }else if(IS_BLACK(b->left)){
-            b->right = right_rotate(b->right);
-            b = left_rotate(b);
-            b->colour = BLACK;
-            b->left->colour= RED;
-        }
-    }
-
-    else if (IS_RED(b->right) && IS_RED(b->right->right)){
-        if (IS_RED(b->left)){
-            b->colour = RED;
-            b->left->colour = BLACK;
-            b->right->colour = BLACK;
-        }else{
-            b = left_rotate(b);
-            b->colour = BLACK;
-            b->left->colour = RED;
-        }
-    }
-
-    return b;
-}
-
-tree tree_insert(tree b, char *str) {
-    int cmp;
-    if (b == NULL) {
-        b = tree_new(tree_type);
-    }
-    
-    if (b->key == NULL) {
-        b->key = emalloc(sizeof(char) * (strlen(str) + 1));
-        strcpy(b->key, str);
-        b->frequency = 1;
-        return b;
-    }
-    
-    cmp = strcmp(str, b->key);
-    if (cmp < 0) {
-        b->left = tree_insert(b->left, str);
-    } else if (cmp > 0) {
-        b->right = tree_insert(b->right, str);
-    } else if (cmp == 0) {
-        b->frequency += 1;
-    }
-    
-    if (tree_type == RBT) {
-        b = tree_fix(b);
-    }
-    
-    return b;
-}
-
-/*
- * returns 0 if not found
- * returns 1 if found
+ * @param t the tree to output the DOT description of.
+ * @param out the stream to write the DOT description to.
  */
-int tree_search(tree b, char *str) {
-    if (b == NULL) {
-        return 0;
-    }
-    if (strcmp(str, b->key) == 0) {
-        return 1;
-    } else if (strcmp(str, b->key) < 0) {
-        return tree_search(b->left, str);
-    } else {
-        return tree_search(b->right, str);
-    }
+void tree_output_dot(tree t, FILE *out) {
+    fprintf(out, "digraph tree {\nnode [shape = Mrecord, penwidth = 2];\n");
+    tree_output_dot_aux(t, out);
+    fprintf(out, "}\n");
 }
-
-void tree_inorder(tree b, void f(char *str)) {
-    if (b == NULL) {
-        return;
-    }
-    tree_inorder(b->left, f);
-    f(b->key);
-    tree_inorder(b->right, f);
-}
-
-void tree_preorder(tree b, void f(int frequency, char *str)) {
-    if (b == NULL) {
-        return;
-    }
-    f(b->frequency, b->key);
-    tree_preorder(b->left, f);
-    tree_preorder(b->right, f);
-}
-
-tree setColourBlack(tree b) {
-    b->colour = BLACK;
-    return b;
-}
-
-tree tree_free(tree b) {
-    if (b == NULL){
-        return b;
-    }else{
-        tree_free(b->left);
-        tree_free(b->right);
-        free(b->key);
-        free(b);
-        return b;
-    }
-}
-
-/**
- * Traverses the tree writing a DOT description about connections, and
- * possibly colours, to the given output stream.
- *
- * @param t the tree to output a DOT description of.
- * @param out the stream to write the DOT output to.
- */
-void tree_output_dot_aux(tree t, FILE *out) {
-    if(t->key != NULL) {
-        fprintf(out, "\"%s\"[label=\"{<f0>%s:%d|{<f1>|<f2>}}\"color=%s];\n",
-                t->key, t->key, t->frequency,
-                (RBT == tree_type && RED == t->colour) ? "red":"black");
-    }
-    if(t->left != NULL) {
-        tree_output_dot_aux(t->left, out);
-        fprintf(out, "\"%s\":f1 -> \"%s\":f0;\n", t->key, t->left->key);
-    }
-    if(t->right != NULL) {
-        tree_output_dot_aux(t->right, out);
-        fprintf(out, "\"%s\":f2 -> \"%s\":f0;\n", t->key, t->right->key);
-    }
-}
-
-/**
- * Output a DOT description of this tree to the given output stream.
- * DOT is a plain text graph description language (see www.graphviz.org).
- * You can create a viewable graph with the command
- *
- *    dot -Tpdf < graphfile.dot > graphfile.pdf
- *
- * You can also use png, ps, jpg, svg... instead of pdf
- *
--- tree.h
+++ tree.h
@@ -2,7 +2,7 @@
 #define TREE_H_
 
 #include <stdio.h>
-
+/* Header file for tree implementation. */
 typedef struct tree_node *tree;
 typedef enum { RED, BLACK } tree_colour;
 typedef enum tree_e { BST, RBT } tree_t;
@@ -13,8 +13,8 @@ extern tree tree_insert(tree r, char *str);
 extern tree tree_new(tree_t type);
 extern void tree_preorder(tree r, void f(int freq, char *str));
 extern int tree_search(tree r, char *key);
-extern tree setColourBlack(tree);
+extern tree setColourBlack(tree t);
 extern void tree_output_dot(tree t, FILE *out);
-extern void tree_output_dot_aux(tree t, FILE *out);
+
 
 #endif /* tree_h */
